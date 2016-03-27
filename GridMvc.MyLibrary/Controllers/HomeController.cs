8429468e151f62e869cec54c260eec32c8507ef7
Gürkan Minaz <gurkanminaz@outlook.com>
2016-03-28 01:25:31 +0300

    Grid MVC kullanımı

---
+++ GridMvc.MyLibrary/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GridMvc.MyLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GridMvc.MyLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Kisi> _sampleKisiler = null;
+        private static int _count = 20;
+        // GET: Home
+        public ActionResult Index(int? id)
+        {
+            if (id == null) id = 20;
+
+            if (_sampleKisiler == null || id != _count)
+            {
+                _count = (int)id;
+                _sampleKisiler = Kisi.GetSampleKisiler(_count);
+            }
+
+            return View(_sampleKisiler);
+        }
+
+        public ActionResult Index2(int? id)
+        {
+            if (id == null) id = 20;
+
+            if (_sampleKisiler == null || id != _count)
+            {
+                _count = (int)id;
+                _sampleKisiler = Kisi.GetSampleKisiler(_count);
+            }
+
+            return View(_sampleKisiler);
+        }
+
+        public JsonResult GetKisilerAjax()
+        {
+            return Json(_sampleKisiler.Select(x => x.FullName).OrderBy(x => x.ToString()));
+        }
+    }
+}
---
+++ GridMvc.MyLibrary/Models/Kisi.cs
@@ -0,0 +1,45 @@
+﻿using GridMvc.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GridMvc.MyLibrary.Models
+{
+    public class Kisi
+    {
+        [GridHiddenColumn()]
+        public int Id { get; set; }
+
+        [GridColumn(Title ="Ad-Soyad",FilterEnabled =true,SortEnabled =true)]
+        public string FullName { get; set; }
+
+        [GridColumn(Title ="Yaş",SortEnabled =true)]
+        public int Age { get; set; }
+
+        [GridColumn(Title ="D.Tarihi",FilterEnabled =true,SortEnabled =true,Format ="{0:dd:MM.yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [NotMappedColumn()] //hiç görünmeyecek, eklenmeyecek
+        public bool IsActive { get; set; }
+
+        public static List<Kisi> GetSampleKisiler(int count = 10)
+        {
+            Random rnd = new Random();
+            List<Kisi> kisiler = new List<Kisi>();
+
+            for (int i = 0; i < count; i++)
+            {
+                kisiler.Add(new Kisi()
+                {
+                    Id = i,
+                    FullName = FakeData.NameData.GetFullName(),
+                    Age = rnd.Next(10, 99),
+                    BirthDate = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-60), DateTime.Now.AddYears(-10)),
+                    IsActive = FakeData.BooleanData.GetBoolean()
+                });
+            }
+            return kisiler;
+        }
+    }
+}
